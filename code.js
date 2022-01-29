5a52de5d22cf3df5dbb3b3db92f1b1ac3878d8b3
max <max@richardparris.com>
2022-01-29 11:34:03 +0800

    max - first files

--- code.js
+++ code.js
@@ -28,15 +28,16 @@ function logkey(e) {
 }
 
 function addLetter(l) {
-  el = document.querySelector('.r' + game.rc + '.c' + game.lc);
-  game.guess.push(l);
-  console.log(game.guess);
-  el.innerHTML = l;
-  game.lc ++;
+    l = l.toUpperCase();
+    el = document.querySelector('.r' + game.rc + '.c' + game.lc);
+    game.guess.push(l);
+    console.log(game.guess);
+    el.innerHTML = l;
+    game.lc++;
 }
 
 function backspace() {
-    game.lc --;
+    game.lc--;
     game.guess.pop();
     el = document.querySelector('.r' + game.rc + '.c' + game.lc);
     el.innerHTML = "";
@@ -46,35 +47,35 @@ var nice;
 
 function enter() {
     console.log('check word');
-    g = game.guess; 
+    g = game.guess;
     nice = g.join("");
     if (allWords.includes(nice.toLowerCase()) == true) {
         colorWord();
-        alert("eet ees a vird!");
     } else alert("That is not a word!");
 }
 
-function wordSel () {
+function wordSel() {
     game.target = targetWords[Math.floor(Math.random() * 202)];
-    game.target.split();
+    game.target = game.target.toUpperCase().split("");
+    console.log(game.target);
 }
 
 var letterDiv;
-function colorWord () {
-    var tword = game.target;
-    for (i  = 0; i <= 4; i++) {
+function colorWord() {
+    var tword = game.target.join("");
+    for (i = 0; i <= 4; i++) {
         letterDiv = document.querySelector(".r" + game.rc + ".c" + i);
-        if(tword[i] == game.guess[i]) {
+        if (tword[i] == game.guess[i]) {
             letterDiv.classList.add('green');
-            tword = tword.replace(game.guess[i],"");
+            tword = tword.replace(game.guess[i], "");
             console.log(tword);
         }
     }
-    for (i  = 0; i <= 4; i++) {
+    for (i = 0; i <= 4; i++) {
         letterDiv = document.querySelector(".r" + game.rc + ".c" + i);
-        if(tword.includes(game.guess[i])) {
+        if (tword.includes(game.guess[i])) {
             letterDiv.classList.add('yellow');
-            tword = tword.replace(game.guess[i],"");
+            tword = tword.replace(game.guess[i], "");
             console.log(tword);
         }
     }
