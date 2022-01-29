b2f66c6be4f12e54df3c3d51771df4f5d3250ce0
max <max@richardparris.com>
2022-01-29 15:27:17 +0800

    update, added keyboard, validation functions, basic functionality working

--- code.js
+++ code.js
@@ -3,11 +3,26 @@ game.target = [];
 game.guess = [];
 game.rc = 0;
 game.lc = 0;
+game.green = [];
 var targetWords;
 var el;
 wordSel();
 var colors = [];
 document.addEventListener('keyup', logkey);
+
+function attachEvents() {
+    var k = {};
+    document.querySelectorAll(".kkey").forEach(kkey => {
+        kkey.addEventListener("click", (e) => {
+            k.key = e.target.id.split("-")[0];
+            k.keyCode = e.target.id.split("-")[1];
+            console.log(k);
+            logkey(k);
+        });
+    });
+}
+
+attachEvents();
 
 function logkey(e) {
     console.log(e.keyCode);
@@ -49,9 +64,11 @@ function enter() {
     console.log('check word');
     g = game.guess;
     nice = g.join("");
-    if (allWords.includes(nice.toLowerCase()) == true) {
+    if (allWords.includes(nice.toLowerCase())) {
         colorWord();
-    } else alert("That is not a word!");
+    } else {
+        alert("That is not a word!");
+    }
 }
 
 function wordSel() {
@@ -62,22 +79,43 @@ function wordSel() {
 
 var letterDiv;
 function colorWord() {
-    var tword = game.target.join("");
+    var tword = [];
+    tword = Array.from(game.target);
+    var gword = [];
+    gword = Array.from(game.guess);
     for (i = 0; i <= 4; i++) {
         letterDiv = document.querySelector(".r" + game.rc + ".c" + i);
-        if (tword[i] == game.guess[i]) {
+        if (tword[i] == gword[i]) {
             letterDiv.classList.add('green');
-            tword = tword.replace(game.guess[i], "");
-            console.log(tword);
+            gword[i] = "-";
+            tword[i] = "_";
         }
     }
-    for (i = 0; i <= 4; i++) {
-        letterDiv = document.querySelector(".r" + game.rc + ".c" + i);
-        if (tword.includes(game.guess[i])) {
+    for (j = 0; j <= 4; j++) {
+        letterDiv = document.querySelector(".r" + game.rc + ".c" + j);
+        if (tword.includes(gword[j])) {
             letterDiv.classList.add('yellow');
-            tword = tword.replace(game.guess[i], "");
-            console.log(tword);
+            tword[i] = "_";
         }
     }
+    if (game.guess.toString() == game.target.toString()) {
+        win();
+    }
+    game.rc++;
+    game.lc = 0;
+    nice = "";
+    g = [];
+    game.guess = [];
+    if (game.rc == 6){
+        loses();
+    }
+}
 
+function win() {
+    alert("Good work! You WIN!");
+    
+}
+
+function loses() {
+    alert("You lossed :( Try again if you want to win");
 }
