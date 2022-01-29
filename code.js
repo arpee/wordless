517003f91f312ab1677f1558486cc7c24843cb6d
max <max@richardparris.com>
2022-01-29 10:00:58 +0800

    max - first files

---
+++ code.js
@@ -0,0 +1,82 @@
+game = {};
+game.target = [];
+game.guess = [];
+game.rc = 0;
+game.lc = 0;
+var targetWords;
+var el;
+wordSel();
+var colors = [];
+document.addEventListener('keyup', logkey);
+
+function logkey(e) {
+    console.log(e.keyCode);
+    console.log(e.keyCode + 3);
+
+    if (e.keyCode == 8) {
+        backspace();
+    }
+    if (e.keyCode == 13 && game.lc == 5) {
+        enter();
+    }
+    if (e.keyCode >= 65 && e.keyCode <= 90 && game.lc <= 4) {
+        console.log("addletter");
+        addLetter(e.key.toUpperCase());
+    }
+    console.log(e);
+    console.log(e.key.toUpperCase());
+}
+
+function addLetter(l) {
+  el = document.querySelector('.r' + game.rc + '.c' + game.lc);
+  game.guess.push(l);
+  console.log(game.guess);
+  el.innerHTML = l;
+  game.lc ++;
+}
+
+function backspace() {
+    game.lc --;
+    game.guess.pop();
+    el = document.querySelector('.r' + game.rc + '.c' + game.lc);
+    el.innerHTML = "";
+}
+var g;
+var nice;
+
+function enter() {
+    console.log('check word');
+    g = game.guess; 
+    nice = g.join("");
+    if (allWords.includes(nice.toLowerCase()) == true) {
+        colorWord();
+        alert("eet ees a vird!");
+    } else alert("That is not a word!");
+}
+
+function wordSel () {
+    game.target = targetWords[Math.floor(Math.random() * 202)];
+    game.target.split();
+}
+
+var letterDiv;
+function colorWord () {
+    var tword = game.target;
+    for (i  = 0; i <= 4; i++) {
+        letterDiv = document.querySelector(".r" + game.rc + ".c" + i);
+        if(tword[i] == game.guess[i]) {
+            letterDiv.classList.add('green');
+            tword = tword.replace(game.guess[i],"");
+            console.log(tword);
+        }
+    }
+    for (i  = 0; i <= 4; i++) {
+        letterDiv = document.querySelector(".r" + game.rc + ".c" + i);
+        if(tword.includes(game.guess[i])) {
+            letterDiv.classList.add('yellow');
+            tword = tword.replace(game.guess[i],"");
+            console.log(tword);
+        }
+    }
+
+}
